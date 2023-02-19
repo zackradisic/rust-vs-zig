f56111aadf5176b999f080df10b419a0b0db0f9a
Zack <zack@theradisic.com>
2023-02-19 23:05:44 +0300

    Run some benchmarks

--- zlox/src/native_fns.zig
+++ zlox/src/native_fns.zig
@@ -4,7 +4,8 @@ const Value = @import("value.zig").Value;
 pub fn clock(arg_count: u8, args: []Value) Value {
     _ = arg_count;
     _ = args;
-    return Value.number(@intToFloat(f64, std.time.timestamp()));
+    // return Value.number(@intToFloat(f64, std.time.timestamp()));
+    return Value.number(420);
 }
 
 pub fn __dummy(arg_count: u8, args: []Value) Value {
